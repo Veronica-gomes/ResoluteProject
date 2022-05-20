f0af61e816ad7dc591c8b216ff79130e9b901dcb
Pedro Henrique <89493619+pedro-costa22@users.noreply.github.com>
2022-05-20 13:33:36 -0300

    Add files via upload

---
+++ _assets/js/index.js
@@ -0,0 +1,57 @@
+/* ---- Marcador de página navbar ---- */
+const currentLocation = location.href;
+const navItems = document.querySelectorAll('a');
+const navLenght = navItems.length;
+
+for(let i = 0; i < navLenght; i++){
+    if(navItems[i].href === currentLocation){
+        navItems[i].className = 'active';
+    }
+} 
+
+
+/* ---- Introdução negocio ---- */
+const divPrincipios = document.querySelector('.principios');
+const divValores = document.querySelector('.valores');
+const divMissoes = document.querySelector('.missoes');
+
+
+divPrincipios.addEventListener("click", () =>{
+    let principiosTitulo = document.querySelector('.principios h3');
+    let principiosText = document.querySelectorAll('.principios p');
+    let principiosBorder = document.querySelector('.principios .linha-vert');
+
+    principiosTitulo.classList.toggle('titutloActive');
+    principiosBorder.classList.toggle('borderActive');
+    for(let i = 0; i<= principiosText.length; i++){
+        principiosText[i].classList.toggle('textoActive');
+    }
+});
+
+divValores.addEventListener("click", () =>{
+    let valoresTitulo = document.querySelector('.valores h3');
+    let valoresText = document.querySelectorAll('.valores p');
+    let valoresBorder = document.querySelector('.valores .linha-vert');
+
+    valoresTitulo.classList.toggle('titutloActive');
+    valoresBorder.classList.toggle('borderActive');
+    for(let i = 0; i <= valoresText.length; i++){
+        valoresText[i].classList.toggle('textoActive');
+    }
+});
+
+divMissoes.addEventListener("click", () =>{
+    let missoesTitulo = document.querySelector('.missoes h3');
+    let missoesText = document.querySelectorAll('.missoes p');
+    let missoesBorder = document.querySelector('.missoes .linha-vert');
+
+    missoesTitulo.classList.toggle('titutloActive');
+    missoesBorder.classList.toggle('borderActive');
+    for(let i = 0; i <= missoesText.length; i++){
+        missoesText[i].classList.toggle('textoActive');
+    }
+
+})
+
+
+
